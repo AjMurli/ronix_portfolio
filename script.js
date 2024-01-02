0df3a9d7ececd6891d696b4d4a93da0ca4306132
Murli <murlipalav3@gmail.com>
2024-01-02 14:09:32 +0530

    Added New design

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
+document.documentElement.style.cursor = 'none';
+
+  // Mouse Over
+  const cursor = document.querySelector(".cursor");
+  var timeout;
+
+  document.addEventListener("mousemove",(e) => {
+  let x = e.pageX;
+  let y = e.pageY;
+  
+  cursor.style.top = y + "px";
+  cursor.style.left = x + "px";
+  cursor.style.display = "block";
+
+  // Cursor Effect On MouseOut
+  function mouseStopped() {
+  cursor.style.display = "none";
+  }
+  clearTimeout(timeout);
+  timeout = setTimeout(mouseStopped, 1000);
+  });
+
+  // Cursor Effect On Mouseout
+  document.addEventListener("mouseout",() => {
+    cursor.style.display = "none";
+  });
+
+
+
 const navBar = document.querySelector("nav"),
        menuBtns = document.querySelectorAll(".menu-icon"),
        overlay = document.querySelector(".overlay");
@@ -22,12 +51,13 @@ const navBar = document.querySelector("nav"),
   });
   
 
-  // 
-  document.addEventListener("DOMContentLoaded", function () {
-    var aboutMeSection = document.getElementById("about-me");
+  
+  const bgAnimation = document.getElementById('bgAnimation');
 
-    document.querySelector(".nav-link").addEventListener("click", function (e) {
-      e.preventDefault(); // Prevent the default link behavior
-      aboutMeSection.style.display = "flex";
-    });
-  });
+  const numberOfColorBoxes = 400;
+  
+  for (let i = 0; i < numberOfColorBoxes; i++) {
+      const colorBox = document.createElement('div');
+      colorBox.classList.add('colorBox');
+      bgAnimation.append(colorBox)
+  }
